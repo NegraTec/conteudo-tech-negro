b404d51cf1d5dcd28e7d34897aad229a0234d82b
Roselma Mendes <roselma.mendes@gmail.com>
2018-11-11 16:55:11 -0200

    Adiciona Flask SQLAlchemy.

--- app.py
+++ app.py
@@ -1,68 +1,75 @@
 from flask import Flask, render_template
 import random
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@db:5432/conteudonegro'
+db = SQLAlchemy(app)
+
+
+class Autora(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(250), nullable=False)
+    redes_sociais = db.Column(db.String(250))
+
+    def __init__(self, nome, redes_sociais):
+        self.nome = nome
+        self.redes_sociais = redes_sociais
+
+    def __repr__(self):
+        return '<Autora %r>' % self.nome
+
+
+class Conteudo(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(250))
+    url = db.Column(db.String(250))
+    resumo = db.Column(db.String(250), nullable=False)
+    data_adicao = db.Column(db.Date)
+    autora_id = db.Column(db.Integer, db.ForeignKey('autora.id'))
+    autora = db.relationship('Autora')
+    # tema = db.Column(db.String(250))
+    # tipo_conteudo = db.Column(db.String(250))
+    # imagem = db.Column(db.String(250))
+
+    def __init__(self, titulo, url, resumo, data_adicao, autora):
+        self.titulo = titulo
+        self.url = url
+        self.resumo = resumo
+        self.data_adicao = data_adicao
+        self.autora = autora
+
+    def __repr__(self):
+        return '<Conteudo %r>' % self.titulo
+
+
+db.create_all()
+
+
+def criar_conteudo_view(conteudos):
+    conteudo_view = []
+    for conteudo in conteudos:
+        autora = Autora.query.filter_by(id=conteudo.autora_id).first()
+        conteudo_view.append(
+            {
+                'titulo': conteudo.titulo,
+                'url': conteudo.url,
+                'resumo': conteudo.resumo,
+                'nome': autora.nome
+            }
+        )
+
+    return conteudo_view
+
 
 @app.route("/")
 def index():
-    conteudos = [
-        {
-            'nome': 'Roselma Mendes',
-            'url': 'https://roselmamendes.github.io/tec/auditoria/',
-            'titulo': 'Logging',
-            'imagem': 'https://roselmamendes.github.io/images//logging/print-log.png',
-            'resumo': 'Muita agua já rolou no meu papel de desenvolvedora, desde 2016 (quando postei pela primeira vez esse post). Agora um texto atualizado sobre a práticas de logs. E se você não loga na sua aplicação, eu aconselho a começar a fazer.',
-            'tema': 'Monitoramento',
-            'redes_sociais': 'twitter: roselmamendes',
-            'data_adicao': '11/11/2018',
-            'tipo_conteudo': 'texto'
-        },
-        {
-            'nome': 'Will Mendes',
-            'url': 'https://medium.com/@willmendesneto/publishing-angular-module-with-np-package-68d6c0650368',
-            'titulo': 'Publishing Angular Module with NP package',
-            'imagem': '',
-            'resumo': '',
-            'tema': 'Frontend',
-            'redes_sociais': 'twitter: willmendesneto'
-        },
-        {
-            'nome': 'Edla Santos',
-            'url': 'https://medium.com/@edlasantos/quando-voc%C3%AA-assume-um-projeto-%C3%A9-pensado-em-v%C3%A1rias-coisas-b4351a13309a',
-            'imagem': '',
-            'resumo': '',
-            'tema': 'Agilidade',
-            'titulo': 'E a inception, como vai?',
-            'redes_sociais': 'twitter: edladossantos1'
-        },
-        {
-            'nome': 'Pedro Henrique',
-            'url': 'https://medium.com/@pedrro/uma-breve-introdu%C3%A7%C3%A3o-a-teste-de-performance-31e788337157',
-            'imagem': '',
-            'resumo': '',
-            'tema': 'Testes',
-            'titulo': 'Uma breve introdução a teste de performance',
-            'redes_sociais': 'twitter: pedrohns_'
-        },
-        {
-            'nome': 'Dani Monteiro',
-            'url': 'http://db4beginners.com/blog/postgresql-no-azure/',
-            'titulo': 'Você conhece o PostgreSQL no Azure?',
-            'imagem': 'http://db4beginners.com/wp-content/uploads/2018/11/ball-bright-close-up-207489-1-800x450.jpg',
-            'resumo': '',
-            'tema': 'Banco de Dados',
-            'redes_sociais': 'twitter: DaniMonteiroDBA'
-        },
-        {
-            'nome': 'John Kagga',
-            'url': 'https://medium.com/the-andela-way/deploying-a-python-flask-app-to-heroku-41250bda27d0',
-            'titulo': 'Deploying a Python Flask app on Heroku',
-            'imagem': 'https://cdn-images-1.medium.com/max/1760/1*FMvHG0OBg1K-qlixcCI-NA.png',
-            'resumo': '',
-            'tema': 'Desenvolvimento Web',
-            'redes_sociais': 'medium: johnkagga',
-            'data_adicao': '11/11/2018',
-            'tipo_conteudo': 'texto'
-        }
-    ]
-    destaque = random.choice(conteudos)
-    return render_template('index.html', conteudos=conteudos, destaque=destaque)
+    conteudos = Conteudo.query.all()
+
+    conteudo_view = criar_conteudo_view(conteudos)
+
+    destaque = random.choice(conteudo_view) if conteudos else None
+
+    return render_template('index.html', conteudos=conteudo_view, destaque=destaque)
