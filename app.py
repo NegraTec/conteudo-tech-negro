fe912b5b75bd2f266b7a57a6ddf92b8387802f9c
Roselma Mendes <roselma.mendes@gmail.com>
2018-11-11 08:49:05 -0200

    Adiciona .env-sample.

--- app.py
+++ app.py
@@ -3,4 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    conteudos = [
+        {
+            'nome': 'Roselma Mendes',
+            'url': 'https://roselmamendes.github.io/tec/auditoria/',
+            'titulo': 'Logging',
+            'imagem': 'https://roselmamendes.github.io/images//logging/print-log.png',
+            'resumo': ''}
+    ]
+    return render_template('index.html', conteudos=conteudos)
